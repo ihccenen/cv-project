43fc2174c204b1ab2ed0800dbda0d29f6b9286ca
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-02-11 19:23:53 -0300

    Added general info form

---
+++ src/components/GenInfoForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default function GenInfoForm({
+  name,
+  email,
+  phoneNumber,
+  handleNameChange,
+  handleEmailChange,
+  handlePhoneNumberChange,
+  onSubmitForm,
+}) {
+  return (
+    <form onSubmit={onSubmitForm}>
+      <label htmlFor="name">
+        Name:
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          required
+          onChange={handleNameChange}
+        />
+      </label>
+
+      <label htmlFor="email">
+        E-mail:
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          required
+          pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+          onChange={handleEmailChange}
+        />
+      </label>
+
+      <label htmlFor="phone-number">
+        Phone Number:
+        <input
+          type="tel"
+          id="phone-number"
+          name="phone-number"
+          value={phoneNumber}
+          required
+          pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"
+          onChange={handlePhoneNumberChange}
+        />
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
