412d41c6c779ced44c3766171394f194262c78d3
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-02-12 15:25:36 -0300

    Added class and css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import EducationalExperience from './components/EducationalExperience';
 import GenInfoSection from './components/GenInfoSection';
 import PracticalExperience from './components/PracticalExperience';
+import './styles/App.css';
 
 export default function App() {
   return (
-    <main>
+    <main className="flex">
       <GenInfoSection />
       <EducationalExperience />
       <PracticalExperience />
