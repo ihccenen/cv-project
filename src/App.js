093835697c9a9fb9a6c67d6d255d1e6118dd28f9
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-02-11 21:29:11 -0300

    Added practical experience component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import EducationalExperience from './components/EducationalExperience';
 import GenInfoSection from './components/GenInfoSection';
+import PracticalExperience from './components/PracticalExperience';
 
 export default function App() {
   return (
     <main>
       <GenInfoSection />
       <EducationalExperience />
+      <PracticalExperience />
     </main>
   );
 }
